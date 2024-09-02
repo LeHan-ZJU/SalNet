38efaf8ff504ce4d3e5361f18417573702c68be5
LeHan-ZJU <hanleucas@gmail.com>
2024-09-02 20:14:09 +0800

    Update save_images.py

--- save_images.py
+++ save_images.py
@@ -2,22 +2,22 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--output_dir", default="" ,help="where to put output files")    #输出结果的路径
+parser.add_argument("--output_dir", default="" ,help="where to put output files")   
 
-parser.add_argument("--output_filetype", default="png", choices=["png", "jpeg"])    #输出图像的格式，默认为.png格式
+parser.add_argument("--output_filetype", default="png", choices=["png", "jpeg"])   
 a = parser.parse_args()
 
 def save_images(fetches, step=None):
-    image_dir = os.path.join(a.output_dir, "images")     #图像输出路径
-    if not os.path.exists(image_dir):      #如果输出路径文件夹不存在，则创建一个
+    image_dir = os.path.join(a.output_dir, "images")   
+    if not os.path.exists(image_dir):    
         os.makedirs(image_dir)
 
     filesets = []
     for i, in_path in enumerate(fetches["paths"]):
         name, _ = os.path.splitext(os.path.basename(in_path.decode("utf8")))
         fileset = {"name": name, "step": step}
-        for kind in ["inputs", "outputs", "targets"]:   #依次存储图像，每组图像包含输入（inputs）、输出（outputs）以及Ground-truth（targets）
-            filename = name + "-" + kind + ".png"      #图像命名，图像格式为png格式
+        for kind in ["inputs", "outputs", "targets"]:  
+            filename = name + "-" + kind + ".png"     
             if step is not None:
                 filename = "%08d-%s" % (step, filename)
             fileset[kind] = filename
