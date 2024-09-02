3ed69fde460b040050cc4e1ee62c4a654404fac3
LeHan-ZJU <hanleucas@gmail.com>
2024-09-02 20:15:31 +0800

    Update ImageProcess.py

--- ImageProcess.py
+++ ImageProcess.py
@@ -7,7 +7,7 @@ def preprocess(image):
         return image * 2 - 1
 
 
-def deprocess(image):    #恢复处理
+def deprocess(image):  
     with tf.name_scope("deprocess"):
         # [-1, 1] => [0, 1]
         return (image + 1) / 2
@@ -15,14 +15,14 @@ def deprocess(image):    #恢复处理
 
 def preprocess_lab(lab):  
     with tf.name_scope("preprocess_lab"):
-        L_chan, a_chan, b_chan = tf.unstack(lab, axis=2)  #lab颜色空间：l_chan为黑白通道，输入范围为[0,100];a_chan和b_chan为色彩通道，取值范围大概为[-110,110]
+        L_chan, a_chan, b_chan = tf.unstack(lab, axis=2) 
         # L_chan: black and white with input range [0, 100]
         # a_chan/b_chan: color channels with input range ~[-110, 110], not exact
         # [0, 100] => [-1, 1],  ~[-110, 110] => [-1, 1]
         return [L_chan / 50 - 1, a_chan / 110, b_chan / 110]
 
 
-def deprocess_lab(L_chan, a_chan, b_chan):  #preprocess的反操作
+def deprocess_lab(L_chan, a_chan, b_chan): 
     with tf.name_scope("deprocess_lab"):
         # this is axis=3 instead of axis=2 because we process individual images but deprocess batches
         return tf.stack([(L_chan + 1) / 2 * 100, a_chan * 110, b_chan * 110], axis=3)
@@ -30,7 +30,7 @@ def deprocess_lab(L_chan, a_chan, b_chan):  #preprocess的反操作
 
 def augment(image, brightness):
     # (a, b) color channels, combine with L channel and convert to rgb
-    a_chan, b_chan = tf.unstack(image, axis=3)   #矩阵分解函数
+    a_chan, b_chan = tf.unstack(image, axis=3)   
     L_chan = tf.squeeze(brightness, axis=3)
     lab = deprocess_lab(L_chan, a_chan, b_chan)
     rgb = lab_to_rgb(lab)
